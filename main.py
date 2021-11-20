b3bf8a2019eddda830d367953bbaf23e9a1b74c0
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-19 20:45:04 -0800

    uncomment my beautiful code

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-# import turtle
-# import csv
-# from turtlewriter import TurtleWriter
+import turtle
+import csv
+from turtlewriter import TurtleWriter
 
-# screen = turtle.Screen()
-# screen.title("U.S. States Game")
-# screen.bgpic("./blank_states_img.gif")
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.bgpic("./blank_states_img.gif")
 
-# turtlewriter = TurtleWriter()
+turtlewriter = TurtleWriter()
 
-# with open("50_states.csv", newline='') as csv_file:
-#     states_list = []
-#     data = csv.reader(csv_file)
-#     for row in data:
-#         states_list.append(row)
-# states_list.pop(0)
+with open("50_states.csv", newline='') as csv_file:
+    states_list = []
+    data = csv.reader(csv_file)
+    for row in data:
+        states_list.append(row)
+states_list.pop(0)
 
-# correct_guesses = 0
+correct_guesses = 0
 
-# while correct_guesses < 50:
-#     answer_state = screen.textinput(
-#         title=f"{correct_guesses}/50 States",
-#         prompt="What's another states' name?"
-#     ).title()
-#     if answer_state == "Exit":
-#         with open("states_to_learn.csv", mode="w") as file:
-#             for state in states_list:
-#                 file.write(state[0])
-#                 file.write("\n")
-#         break
-#     for item in states_list:
-#         state_name = str(item[0])
-#         state_x = int(item[1])
-#         state_y = int(item[2])
-#         if answer_state == state_name:
-#             turtlewriter.update_map(state_x, state_y, state_name)
-#             correct_guesses += 1
-#             states_list.remove(item)
+while correct_guesses < 50:
+    answer_state = screen.textinput(
+        title=f"{correct_guesses}/50 States",
+        prompt="What's another states' name?"
+    ).title()
+    if answer_state == "Exit":
+        with open("states_to_learn.csv", mode="w") as file:
+            for state in states_list:
+                file.write(state[0])
+                file.write("\n")
+        break
+    for item in states_list:
+        state_name = str(item[0])
+        state_x = int(item[1])
+        state_y = int(item[2])
+        if answer_state == state_name:
+            turtlewriter.update_map(state_x, state_y, state_name)
+            correct_guesses += 1
+            states_list.remove(item)
 
-# turtle.mainloop()
+turtle.mainloop()
 
 
 # import pandas
